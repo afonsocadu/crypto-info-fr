57f497747bf2279c45c1121503099adf7b5ee927
Carlos Eduardo <afonso.cadu@gmail.com>
2024-12-31 00:24:25 +0000

    Create structure of login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from './api.service';
 import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [CommonModule, LoginComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  protected _email: string = '';
+  protected _password: string = '';
+  protected _password_confirmation: string = '';
+  protected _isCreatingUser: boolean = false;
+
+  
+  /**
+   * Logs in as a recruiter by creating a new user with a unique email and default password.
+   * Upon successful user creation, submits the login form.
+   */
+  protected _logInRecruiter() {
+    this._email = this._generateUniqueEmail();
+    this._password = '123456'
+    this._password_confirmation = '123456'
+  }
+
+  protected _toggleForm() {
+    this._isCreatingUser = !this._isCreatingUser;
+  }
+
+  /**
+   * Handles the login form submission.
+   */
+  protected onSubmit() {
+    
+  }
+
+    
+  /**
+   * Generates a unique email
+   */
+  private _generateUniqueEmail(): string {
+    const timestamp = new Date().getTime();
+    return `recruiter${timestamp}@example.com`; 
+  }
+
+  
 }
